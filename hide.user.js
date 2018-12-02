63abcfbf6813dd20f5ce220db2bfe0e8d63e330e
jarograv <jgraveley@hotmail.com>
2018-12-02 14:18:38 -0500

    Update hide.user.js

--- hide.user.js
+++ hide.user.js
@@ -10,3 +10,4 @@ GM_addStyle('.questlog_entrie#open_quest_2971 { display: none }'); //hide quest 
 GM_addStyle('.questlog_entrie#open_quest_130 { display: none }'); //hide quest that takes your hernandos sword
 GM_addStyle('.questlog_entrie#open_quest_64 { display: none }'); //hide maya quest that blocks you from getting note #3 in hernando little things (used for hernandos)
 GM_addStyle('.questlog_entrie#open_quest_240 { display: none }'); //hide path #2 of raid that doesnt give key
+GM_addStyle('.questlog_entrie#open_quest_152 { display: none }'); //hides path #1 of the decision as path #2 is used for hernando notes
